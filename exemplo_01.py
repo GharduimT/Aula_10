19c7a453971955844320a31a0d6b6b6ba53a0857
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-13 19:22:44 -0300

    utlizando IMPORT RANDOM

---
+++ exemplo_01.py
@@ -0,0 +1,41 @@
+#utilização ou uso de bibliotecas
+#biblioteca de acesso randômico
+#para numeros aleatorios
+
+# import random
+# n = random.randint(1, 10)
+# print(n) '''vai gerar numéros de forma aleatória dentro desse intervalo 1, 10 início e fim'''
+
+# import random
+# n = random.randint(1, 5)
+# m = random.randint(1, 5)
+# print(n, m)
+
+# import random
+# n = random.randint(1, 5)
+# m = random.randint(1, 5)
+# print(n, m)
+
+# list_numbers = [random.randint(1, 10)for i in range(5)]
+# print(list_numbers)
+
+# import random
+# import os
+# n = random.randint(1, 5)
+# m = random.randint(1, 5)
+# print(n, m)
+
+# os.system('cls')
+# list_numbers = [random.randint(1, 10)for i in range(5)]
+# print(list_numbers)
+
+import random
+import os
+
+os.system('cls')
+n = random.randint(1, 5)
+m = random.randint(1, 5)
+print(n, m)
+
+list_numbers = [random.randint(1, 10)for i in range(5)]
+print(list_numbers)

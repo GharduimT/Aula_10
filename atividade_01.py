dbbb9ed7cab368a45eaebb18290c3ae2b1fb6113
GharduimT <gabriel.harduim.ti@gmail.com>
2025-05-13 21:21:39 -0300

    CONCLUÍDO RECEBER NÚMEROS ALEATÓRIOS

--- atividade_01.py
+++ atividade_01.py
@@ -1,6 +1,39 @@
 # criar (com função) calculadora para dois números digitados pelo usuário.
 # a calculadora vai somar, subtrai, divide e multiplica
 
+# def soma(a, b):
+#     return a + b
+
+
+# def subtrai(a, b):
+#     return a - b
+
+
+# def divide(a, b):
+#     # if b == 0:
+#     #     return
+
+#     return a / b
+
+
+# def multiplica(a, b):
+#     return a * b
+
+
+# num1 = int(input('digite o primeiro numero: '))
+# num2 = int(input('digite o segundo numero: '))
+
+# adicao = soma(num1, num2)
+# sub = subtrai(num1, num2)
+# div = divide(num1, num2)
+# mult = multiplica(num1, num2)
+
+# print (adicao, sub, div, mult)
+
+#receber numeros aleatórios
+import random
+
+
 def soma(a, b):
     return a + b
 
@@ -19,15 +52,16 @@ def divide(a, b):
 def multiplica(a, b):
     return a * b
 
-
-num1 = int(input('digite o primeiro numero: '))
-num2 = int(input('digite o segundo numero: '))
+# num1 = int(input('digite o primeiro numero: '))
+# num2 = int(input('digite o segundo numero: '
+num1 = random.randint(1, 10)
+num2 = random.randint(1, 10)
 
 adicao = soma(num1, num2)
 sub = subtrai(num1, num2)
 div = divide(num1, num2)
 mult = multiplica(num1, num2)
 
-print (adicao, sub, div, mult)
+print('Estes são os números escolhidos aleatóriamente', num1, 'e', num2)
+print('SOMA', adicao, '\n', 'SUBTRAÇÃO', sub, '\n', 'DIVISÃO', div, '\n', 'MULTIPLICAÇÃO', mult)
 
-#receber numeros aleatórios
